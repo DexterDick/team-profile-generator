fe8b36a3aa8b6ae1c6917e8db63fde7e2a7c4834
Dexter Dick <@dexterdick@hotmail.com>
2022-12-15 21:55:07 -0500

    made updates to __tests__ removing duplicates

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,24 +1,21 @@
 // import Manager class form lib
 const Intern = require("../lib/Intern");
 
+const intern = new Intern("Dexter", 25, "dexter@dexter.com", "UofT");
 describe("Create Intern Object", () => {
     it("should creat a new Intern", () => {
-        const intern = new Intern("Dexter", 25, "dexter@dexter.com", "UofT");
         expect(intern.school).toEqual("UofT");
     });
 });
 
 describe("School", () => {
     it("should return school or intern", () => {
-        const intern = new Intern("Dexter", 25, "dexter@dexter.com", "UofT");
         expect(intern.getSchool()).toEqual("UofT");
     });
 });
 
 describe("Role", () => {
     it("should return role of intern", () => {
-        const intern = new Intern("Dexter", 25, "dexter@dexter.com", "UofT");
-
         expect(intern.getRole()).toEqual("Intern");
     });
 });
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,16 +1,15 @@
 // import Manager class form lib
 const Manager = require("../lib/Manager");
+const manager = new Manager("Dexter", 25, "dexter@dexter.com", 50);
 
 describe("Create Manager Object", () => {
     it("should creat a new manager", () => {
-        const manager = new Manager("Dexter", 25, "dexter@dexter.com", 50);
         expect(manager.officeNumber).toEqual(50);
     });
 });
 
 describe("Manager role", () => {
     it("should return employee role", () => {
-        const manager = new Manager("Dexter", 25, "dexter@dexter.com", 50);
         expect(manager.getRole()).toEqual("Manager");
     });
 });
